d5ca47344d264f14b0d7d2ef258bde4cea7c7fd9
arximughal <hello@arslanaslam.me>
2022-12-10 01:18:22 +0500

    fix version update

--- bravo-screens-custom-pricing.php
+++ bravo-screens-custom-pricing.php
@@ -16,7 +16,7 @@
  * Plugin Name:       BravoScreens Custom Pricing
  * Plugin URI:        bravo-screens-custom-pricing
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area. (4.0.0 - Added extra description fields for product category pages)
- * Version:           4.0.0
+ * Version:           4.0.1
  * Author:            Muhammad Arslan Aslam
  * Author URI:        https://arslanaslam.me
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('BRAVO_SCREENS_CUSTOM_PRICING_VERSION', '4.0.0');
+define('BRAVO_SCREENS_CUSTOM_PRICING_VERSION', '4.0.1');
 
 /**
  * The code that runs during plugin activation.
