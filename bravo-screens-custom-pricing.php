d1c47719ae3ea905a4019d0b1ec6eae10da2aa75
arximughal <hello@arslanaslam.me>
2023-02-27 00:20:55 +0500

    bump up the version

--- bravo-screens-custom-pricing.php
+++ bravo-screens-custom-pricing.php
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('BRAVO_SCREENS_CUSTOM_PRICING_VERSION', '4.2.2');
+define('BRAVO_SCREENS_CUSTOM_PRICING_VERSION', '4.3.1');
 
 /**
  * The code that runs during plugin activation.
